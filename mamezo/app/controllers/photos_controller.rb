a897d89b729a9491666812eb210d0b0f8d053d2b
itakurara <kaiamira26@gmail.com>
2015-06-23 07:01:50 +0900

    データを登録する画面を用意

--- mamezo/app/controllers/photos_controller.rb
+++ mamezo/app/controllers/photos_controller.rb
@@ -1,4 +1,18 @@
 class PhotosController < ApplicationController
   def new
+    @photo = Photo.new
   end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def photo_params
+      params.require(:photo).permit(:name, :comment)
+    end
 end
--- mamezo/config/routes.rb
+++ mamezo/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :photos
   get 'photos/new'
-
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
