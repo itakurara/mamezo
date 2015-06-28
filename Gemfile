5f4c67121274aba8ca3515fcb288b938f11cbc7e
itakurara <kaiamira26@gmail.com>
2015-06-28 09:44:05 +0900

    私だけ写真が投稿できるように認証実装

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '3.3.5'
 gem 'masonry-rails'
+gem 'bcrypt-ruby', '3.1.2'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   resources :photos do
     member do
       get 'show_image'
     end
   end
   root 'static_pages#home'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
