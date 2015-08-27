75619c33273ba1f286316b2027bd626bdb93409d
板倉由依 <itakura@plus-d.co.jp>
2015-08-27 16:02:39 +0900

    最新のものから表示

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.page(params[:page]).per(10)
+    @photos = Photo.order("created_at DESC").page(params[:page]).per(10)
   end
 
   def new
