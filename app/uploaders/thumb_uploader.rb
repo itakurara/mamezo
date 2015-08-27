33b5b4ddc5ae34e8698a8ffc1d8fc97c4554b3a3
板倉由依 <itakura@plus-d.co.jp>
2015-08-27 15:48:59 +0900

    画像がNOT FOUNDになる現象を解決するためstorageをコメントアウト

--- app/uploaders/thumb_uploader.rb
+++ app/uploaders/thumb_uploader.rb
@@ -8,14 +8,14 @@ class ThumbUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave 
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   process :resize_to_limit => [290, nil]
   #process :fix_rotate
@@ -26,6 +26,10 @@ class ThumbUploader < CarrierWave::Uploader::Base
   #    img = yield(img) if block_given?
   #    img
   #  end
+  #end
+
+  # def public_id
+  #  return model.id
   #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
